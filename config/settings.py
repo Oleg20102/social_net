f47b7afe81df20b8c86e554c8c885839cbe3bed2
Oleg20102 <olegkhavrona@gmail.com>
2026-03-14 11:16:22 +0200

    social

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-%qs-+l@gu1$9d)8z^5xxj($i@n55c6^&mkhrhlul!ct@!n2fl-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
